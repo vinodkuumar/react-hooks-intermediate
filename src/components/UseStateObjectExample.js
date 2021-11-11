a18f3e4b4cbeb8a0576e042c69d8e8b2f83d929d
VinodKumar Muppera <vinod.muppera@vedantu.com>
2021-11-11 11:56:40 +0530

    usestate with objects

---
+++ src/components/UseStateObjectExample.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+
+const UseStateObjectExample = () => {
+    const [name,setName] = useState({firstName: '', lastName: ''});
+    return(
+        <>
+        <form>
+            <input 
+                type="text"
+                value={name.firstName}
+                onChange={(e) => setName({firstName: e.target.value})}
+                />
+            <input 
+                type="text"
+                value={name.lastName}
+                onChange={(e) => setName({lastName: e.target.value})}
+                />
+            <h2>Your first name is :- {name.firstName}</h2>
+            <h2>Your last name is :- {name.lastName}</h2>
+        </form>
+        </>
+    )
+}
+
+export default UseStateObjectExample;
